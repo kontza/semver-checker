258937706f4e9a2177e2d2b527620af34a9348d2
Juha Ruotsalainen <juha.ruotsalainen@ambientia.fi>
2024-07-12 12:25:17 +0300

    Initial version

---
+++ cmd/root-runner.go
@@ -0,0 +1,13 @@
+package cmd
+
+import (
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+	"golang.org/x/mod/semver"
+)
+
+func rootRunner(cmd *cobra.Command, args []string) {
+	versions := []string{"v3.4.0", "v3.11.0", "v4.0.0", "v3.7.0", "v3.8.0", "v3.9.0", "v3.10.0", "v3.12.0"}
+	semver.Sort(versions)
+	log.Info().Strs("versions", versions).Send()
+}
---
+++ cmd/root.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "semver-testi",
+	Short: "semver-koeajoa",
+	Run:   rootRunner,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
+		zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMs
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.StampMilli})
+		return nil
+	}
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
